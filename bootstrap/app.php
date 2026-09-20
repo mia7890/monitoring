1077299ca059671e55186b37d9e8f91383034ee1
Sandree Antang <antang.sandree.lebico@gmail.com>
2026-09-20 11:04:04 +0800

    fix: trust reverse proxies and force HTTPS for forms and assets

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'auth.monitoring' => \App\Http\Middleware\RequireMonitoringAccess::class,
             'admin.monitoring' => \App\Http\Middleware\RequireAdminRole::class,
